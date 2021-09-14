2a8db7011d55d018e521d47ecbb7fc067a5b74ab
rafi.zuhud@gmail.com <rafi.zuhud@gmail.com>
2021-09-15 01:20:47 +0700

    menambahkan Contoh1.php

---
+++ Contoh1.php
@@ -0,0 +1,16 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Rafi Zuhud Agungsyah
+            Saya tinggal di daerah Sukatani
+            Olahraga yang saya sukai adalah 
+            Renang";
+    }
+
+
+
+
+} 
